2e5cbafa8ece4723d99476a6730ce2e9d80b5dda
BuildTools <ray55_king@yahoo.com>
2017-08-06 09:32:59 -0700

    Uploading Alberts modifications to Map Activity

--- Workout-master/app/src/main/java/com/example/calvin/workout/MapActivity.java
+++ Workout-master/app/src/main/java/com/example/calvin/workout/MapActivity.java
@@ -2,7 +2,9 @@ package com.example.calvin.workout;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -28,6 +30,12 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
  * Created by FugiBeast on 8/5/2017.
@@ -35,13 +43,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     GoogleMap mMap;
+    ArrayList<LatLng> points;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
-    private Location mLastLocation;
+    Location oldLocation;
+    double distance = 0d;
+    double distanceCovered = 0;
     private Marker mCurrLocationMarker;
 
     Button startBtn;
-    TextView tv;
+    TextView calcDist;
+    TextView caloriesBurnt;
+    double totalCalories = 0;
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
@@ -49,6 +62,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        points = new ArrayList<LatLng>();
 
         startBtn = (Button) findViewById(R.id.startBtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
@@ -62,23 +77,40 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 }
             }
         });
-        tv = (TextView)findViewById(R.id.calcDistance);
-        tv.setText("Put current distance here");
+        calcDist = (TextView)findViewById(R.id.calcDistance);
+        calcDist.setText("Put current distance here");
+        caloriesBurnt = (TextView)findViewById(R.id.caloriesBurnt);
+        caloriesBurnt.setText("Calories burned : "+totalCalories);
 
+        oldLocation= new Location("dummy data");
         checkLocationPermission();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
+            Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                    .clickable(false)
+                    .add(   new LatLng(34.065909, -118.168578),
+                            new LatLng(34.065827, -118.168613),
+                            new LatLng(34.065676, -118.168688),
+                            new LatLng(34.065525, -118.168720),
+                            new LatLng(34.065502, -118.168758),
+                            new LatLng(34.065432, -118.168693),
+                            new LatLng(34.065339,-118.168633),
+                            new LatLng(34.065262,-118.168515),
+                            new LatLng(34.065268,-118.168417),
+                            new LatLng(34.065333,-118.168354),
+                            new LatLng(34.065395,-118.168386),
+                            new LatLng(34.065406,-118.168550))
+                    .color(Color.BLUE)
+                    .width(8));
         //Initialize Google Play Services
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this,
@@ -92,6 +124,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             buildGoogleApiClient();
             mMap.setMyLocationEnabled(true);
         }
+
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -106,8 +139,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(1000);
-        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setInterval(1000*5);
+        mLocationRequest.setFastestInterval(1000*5);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
@@ -128,21 +161,33 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onLocationChanged(Location location) {
-        mLastLocation = location;
         if (mCurrLocationMarker != null) {
             mCurrLocationMarker.remove();
+        }
+        if(oldLocation.getLatitude() == 0){
+            oldLocation = location;
+        }
+        if(location != null){
+            points.add(new LatLng(location.getLatitude(), location.getLongitude()));
+            distance = oldLocation.distanceTo(location);
+            oldLocation = location;
+            distanceCovered = distanceCovered + distance;
+            calcDist.setText("Distance Covered : "+distanceCovered +" meters");
+            //calculate calories burnt here
+            //caloriesBurnt.setText("Calories burned : "+totalCalories);
         }
 
         //Place current location marker
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        LatLng demo = new LatLng(34.065406,-118.168550);
+        points.add(latLng);
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
+        markerOptions.position(demo);
         markerOptions.title("Current Position");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
         mCurrLocationMarker = mMap.addMarker(markerOptions);
 
         //move map camera
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(demo));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(30));
 
         //stop location updates
